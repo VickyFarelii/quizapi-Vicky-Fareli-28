8fd02aa5658958cd1de3897ae2576d5d35a0e477
VickyFarelii <vickyfareli2000@gmail.com>
2024-01-18 08:30:27 +0700

    first commit

---
+++ models/quiz.js
@@ -0,0 +1,32 @@
+module.exports = (sequelize, Sequelize) => {
+    const Quiz = sequelize.define('quiz', {
+        quiz: {
+            type: Sequelize.STRING,
+        },
+        a: {
+            type: Sequelize.STRING,
+        },
+        b: {
+            type: Sequelize.STRING,
+        },
+        c: {
+            type: Sequelize.STRING,
+        },
+        d: {
+            type: Sequelize.STRING,
+        },
+        e: {
+            type: Sequelize.STRING,
+        },
+        key: {
+            type: Sequelize.STRING,
+        },
+        categoryId: {
+            type: Sequelize.STRING,
+        },
+        levelId: {
+            type: Sequelize.STRING,
+        },
+    });
+    return Quiz;
+}
